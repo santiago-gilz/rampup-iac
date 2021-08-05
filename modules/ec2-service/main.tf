44ff2a2cf14817501fdaf5f54940d37c6458bdbb
Santiago Gil Zapata <santiago.gilz@perficient.com>
2021-08-05 13:07:04 -0500

    Well formatted

--- modules/ec2-service/main.tf
+++ modules/ec2-service/main.tf
@@ -36,8 +36,8 @@ module "asg" {
   user_data = templatefile(
     "./modules/ec2-service/templates/run_ansible.sh.tpl",
     {
-      "TARGET_APP"      = var.target_app
-      "extra_vars"      = jsonencode(
+      "TARGET_APP" = var.target_app
+      "extra_vars" = jsonencode(
         {
           "api_lb_ip"       = var.api_lb_ip
           "api_access_port" = var.API_ACCESS_PORT
